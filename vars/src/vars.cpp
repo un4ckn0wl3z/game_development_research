eb5c9b15debeb81d4513a0f97f55bbb96c8d3c4d
un4ckn0wl3z <slickerth@gmail.com>
2019-05-11 13:59:05 +0700

    add guesser

---
+++ vars/src/vars.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : variables.cpp
+// Author      : Anuwat
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x; // 10 , 3,  3.1
+
+	x = 10;
+
+	cout << "The value of x is : " << x << endl;
+
+
+	return 0;
+}
